4fbdb4ed465ac365c0618c604720aaea669858aa
Roberto Souza <robertosouza57@yahoo.com.br>
2022-07-15 22:51:52 -0300

    integração do frontend com o backend

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,21 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function NotificationButton({saleId} : Props) {
+
+    function handleClick(id: number){
+        axios.get(`${BASE_URL}/sales/${id}/notification`).then(res =>{toast.info("SMS enviado com sucesso!")})
+    }
+
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
